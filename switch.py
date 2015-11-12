468115c3b0d85bb4ae2c9b8d3dd6eca69ad3d45c
Clark Phillips <clark.r.phillips@gmail.com>
2015-11-11 23:53:46 -0700

    2 switches NC or NO

--- switch.py
+++ switch.py
@@ -12,9 +12,20 @@ GPIO.setmode(GPIO.BCM)
 # pull_up_down: GPIO.PUD_UP vs GPIO.PUD_DOWN
 # PUD = Pull Up Down
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    input_state = GPIO.input(17)
-    if input_state == False:
-        print('Button Pressed')
+    # GND to COM
+    # G17 to NC (normally connected)
+    pin_17 = GPIO.input(17)
+    if pin_17 == True:
+        print('GPIO #17 button pressed')
         time.sleep(0.2)
+
+    # GND to COM
+    # G18 to NO (normally open)
+    pin_18 = GPIO.input(18)
+    if pin_18 == False:
+        print('GPIO #18 button pressed')
+        time.sleep(0.2)
+
